423780be85bf7dfc5ee652319b1cd94547b6a008
Camila <cami.otto@hotmail.com>
2026-02-15 17:36:07 -0300

    Subindo o projeto da SPRINT 7

--- data.py
+++ data.py
@@ -1,1 +1,8 @@
 # Arquivo com constantes
+URBAN_ROUTES_URL = 'https://cnt-2e279120-3a2b-4448-a21f-fd6fc97501ea.containerhub.tripleten-services.com?lng=pt'
+ADDRESS_FROM = 'East 2nd Street, 601'
+ADDRESS_TO = '1300 1st St'
+PHONE_NUMBER = '+1 123 123 12 12'
+CARD_NUMBER = '1234 5678 9100'
+CARD_CODE = '1111'
+MESSAGE_FOR_DRIVER = 'Pare no bar de sucos'
